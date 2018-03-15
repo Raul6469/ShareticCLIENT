de01a83bace2807233a5be41b6eac860da403e42
Victor <vb4007@gmail.com>
2018-03-15 21:08:19 +0100

    Generate component qcm editor

---
+++ src/app/components/qcm-editor/qcm-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qcm-editor',
+  templateUrl: './qcm-editor.component.html',
+  styleUrls: ['./qcm-editor.component.css']
+})
+export class QcmEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
